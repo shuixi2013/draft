7d267d1d23a1f1e0223736ed10c11b9f7fb4da7d
Li Zheng <lizheng_w5625@tp-link.com.cn>
2017-05-10 12:37:48 +0800

    update tcp stat

--- tcp_stat/statistic.c
+++ tcp_stat/statistic.c
@@ -1,3 +1,11 @@
+/********************************************************************
+ * Copyright (c) 2017, TENCENT TECHNOLOGIES CO., LTD.
+ *
+ * Brief        : Statistics of TCP stream, based on tcpdump.
+ * Author       : zaynli
+ * Created Date : 2017-05-10
+ ********************************************************************/
+
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
@@ -5,8 +13,11 @@
 #include <netdissect-stdinc.h>
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <netinet/in.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <netdissect-stdinc.h>
 
 #include "netdissect.h"
@@ -14,12 +25,15 @@
 #include "tcp.h"
 #include "ip.h"
 
-/************************************************
+/********************************************************************
  * defines and types
- */
-#define OK				(0)
-#define ERR				(-1)
-#define OUTPUT_FILE		"/tmp/capture_tool_output"
+ ********************************************************************/
+#define OK						(0)
+#define ERR						(-1)
+#define FILENAME_LEN			(50)
+#define STREAM_LEN_THRESHOLD	(256 * (2<<10)) /* 256 kB */
+#define OUTPUT_PATH				"/tmp/statistic/"
+#define OUTPUT_SYN_FILE			OUTPUT_PATH"syn_collection"
 
 typedef struct _TUPLE
 {
@@ -27,29 +41,51 @@ typedef struct _TUPLE
 	uint16_t sport, dport;
 } TUPLE;
 
+typedef enum _STREAM_STATE
+{
+	STREAM_START = 0,
+	STREAM_RUNNING,
+	STREAM_END,
+	STREAM_CLEANUP
+} STREAM_STATE;
+
 typedef struct _STREAM
 {
 	TUPLE tuple;
-	time_t time;
+	time_t timestamp;
 	FILE *fp;
-	int total_len; /* TCP payload only */
+	uint32_t total_len; /* TCP payload only */
+	STREAM_STATE state;
 	struct _STREAM *next;
 } STREAM;
 
 
-/************************************************
+/********************************************************************
  * function declares
- */
+ ********************************************************************/
+static STREAM *stream_list_add(STREAM *node);
+static STREAM *stream_list_lookup_by_pointer(STREAM *node);
+static STREAM *stream_list_lookup_by_tuple(TUPLE *tuple);
+static STREAM *stream_list_remove(STREAM *node);
+static STREAM *stream_node_create(void);
+static void stream_node_destroy(STREAM *node);
+static void stream_node_update(
+		struct in_addr saddr, struct in_addr daddr,
+		register const u_char *tcphdr, uint32_t length);
 
-
-/************************************************
+/********************************************************************
  * global variables
- */
+ ********************************************************************/
 STREAM g_stream_head = {0};
 static FILE *g_output = NULL;
 static STREAM *g_stream_list = NULL;
 
-void statistic_cap_tcp(register const u_char *tcphdr, register const u_char *iphdr)
+
+/********************************************************************
+ * function defines
+ ********************************************************************/
+void statistic_cap_tcp(register const u_char *tcphdr, uint32_t length,
+		register const u_char *iphdr, int fragmented)
 {
 	uint16_t sport, dport, win, urp;
 	uint32_t seq, ack, thseq, thack;
@@ -88,9 +124,104 @@ void statistic_cap_tcp(register const u_char *tcphdr, register const u_char *iph
 		fprintf(g_output, ">%s:%d", inet_ntoa(daddr), dport);
 		fprintf(g_output, " %d %u\n", win, seq);
 	}
+
+	length -= TH_OFF(tp) * 4;
+	stream_node_update(saddr, daddr, tcphdr, length);
+		fprintf(stderr, "------------%d\n", __LINE__);
 }
 
-static STREAM *stream_create(void)
+static void stream_node_update(
+		struct in_addr saddr, struct in_addr daddr,
+		register const u_char *tcphdr, uint32_t length)
+{
+	register const struct tcphdr *tp;
+	register u_char flags;
+	uint16_t sport, dport, win, urp;
+
+	tp = (const struct tcphdr *)tcphdr;
+
+	flags = tp->th_flags;
+	sport = EXTRACT_16BITS(&tp->th_sport);
+	dport = EXTRACT_16BITS(&tp->th_dport);
+
+	TUPLE tuple = {
+		.saddr = saddr,
+		.daddr = daddr,
+		.sport = sport,
+		.dport = dport
+	};
+
+	/* handle the stream node in memory */
+	STREAM *node = stream_list_lookup_by_tuple(&tuple);
+	if (node) {
+		/* get this node self */
+		node = node->next;
+
+		if (node->state >= STREAM_END) {
+			goto handle_state;
+		}
+
+		node->total_len += length;
+	} else {
+		if (flags & TH_FIN) {
+			/* did not track it */
+			return;
+		}
+
+		char filename[NAME_MAX] = {0};
+		node = stream_node_create();
+		if (!node) {
+			return;
+		}
+
+		memcpy(&node->tuple, &tuple, sizeof(TUPLE));
+		node->timestamp = time(NULL);
+		node->total_len = length;
+		node->state = STREAM_RUNNING;
+		node->next = NULL;
+		/*
+		 * store the tuple info to file name asume
+		 * file name: sip-sport_dip-dport_timestamp
+		 */
+		sprintf(filename, "%s", OUTPUT_PATH);
+		sprintf(filename, "%s%s:%d", filename, inet_ntoa(node->tuple.saddr), node->tuple.sport);
+		sprintf(filename, "%s_%s:%d", filename, inet_ntoa(node->tuple.daddr), node->tuple.dport);
+		sprintf(filename, "%s_%lx", filename, node->timestamp);
+		fprintf(stderr, "%d------------file name :%s \n", __LINE__, filename);
+		node->fp = fopen(filename, "a");
+		if (!node->fp) {
+			fprintf(stderr, "unable to open file %s\n", filename);
+			return;
+		}
+
+		stream_list_add(node);
+	}
+
+	/* store the stream node to file */
+	fprintf(node->fp, "%d %u\n", win, length);
+
+handle_state:
+	/* handle the stream node state */
+	if (node->total_len >= STREAM_LEN_THRESHOLD) {
+		node->state = STREAM_END;
+	}
+
+	if (flags & TH_FIN) {
+		node->state = STREAM_CLEANUP;
+	}
+
+	if (node->state == STREAM_END) {
+		if (node->fp) {
+			fflush(node->fp);
+			fclose(node->fp);
+			node->fp = NULL;
+		}
+	} else if (node->state == STREAM_CLEANUP) {
+		stream_list_remove(node);
+	}
+}
+
+static STREAM *stream_node_create(void)
 {
 	STREAM *stream = (STREAM *)malloc(sizeof(STREAM));
 	if (!stream) {
@@ -102,15 +233,15 @@ static STREAM *stream_create(void)
 	return stream;
 }
 
-static void stream_destroy(STREAM *node)
+static void stream_node_destroy(STREAM *node)
 {
 	if (node->fp) {
-		fflush(fp);
-		fclose(fp);
+		fflush(node->fp);
+		fclose(node->fp);
+		node->fp = NULL;
 	}
 
 	free(node);
-	node = NULL;
 }
 
 static STREAM *stream_list_add(STREAM *node)
@@ -131,10 +262,16 @@ static STREAM *stream_list_remove(STREAM *node)
 
 	pre = stream_list_lookup_by_pointer(node);
 	next = node->next;
+
+	pre->next = next;
+
+	stream_node_destroy(node);
+
+	return g_stream_list;
 }
 
 /* do not check member */
-/* return the parent pointer */
+/* return the parent's pointer */
 static STREAM *stream_list_lookup_by_pointer(STREAM *node)
 {
 	STREAM *p = g_stream_list;
@@ -151,17 +288,30 @@ static STREAM *stream_list_lookup_by_pointer(STREAM *node)
 	return result;
 }
 
-/* return the parent pointer */
+/* return the parent's pointer */
 static STREAM *stream_list_lookup_by_tuple(TUPLE *tuple)
 {
+	STREAM *p = g_stream_list;
+	STREAM *result = NULL;
+
+	while (p->next) {
+		if (memcmp(tuple, &(p->next->tuple), sizeof(TUPLE)) == 0) {
+			result = p;
+			break;
+		}
+		p = p->next;
+	}
+
+	return result;
 }
 
 void statistic_init(void)
 {
+	mkdir(OUTPUT_PATH, 0775);
 	if (!g_output) {
-		g_output = fopen(OUTPUT_FILE, "a");
+		g_output = fopen(OUTPUT_SYN_FILE, "a");
 		if (!g_output) {
-			fprintf(stderr, "unable to open file %s\n", OUTPUT_FILE);
+			fprintf(stderr, "unable to open file %s\n", OUTPUT_SYN_FILE);
 			return;
 		}
 	}
@@ -172,9 +322,17 @@ void statistic_init(void)
 void statistic_exit(void)
 {
 	if (g_output) {
-		(void)fflush(g_output);
+		fflush(g_output);
 		fclose(g_output);
 		g_output = NULL;
 	}
-}
 
+	STREAM *p = g_stream_list;
+	STREAM *next = p->next;
+	while (next) {
+		p = next;
+		next = p->next;
+
+		stream_node_destroy(p);
+	}
+}
--- tcp_stat/statistic.h
+++ tcp_stat/statistic.h
@@ -1,7 +1,8 @@
 #ifndef STATISTIC_H
 #define STATISTIC_H
 
-void statistic_cap_tcp(register const u_char *tcphdr, register const u_char *iphdr);
+void statistic_cap_tcp(register const u_char *tcphdr, uint32_t length,
+		register const u_char *iphdr, int fragmented);
 void statistic_init(void);
 void statistic_exit(void);
 
